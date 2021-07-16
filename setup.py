427d969d058b524549bd308e6d4d38223e096160
ryanmanzer <rmanzer@nordicnaturals.com>
2021-07-16 12:12:13 -0700

    Updating function to prioritize config file options

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 import setuptools
 
-with open('README.md','r') as ld:
-  long_description = ld.read()
+with open("README.md", "r") as ld:
+    long_description = ld.read()
 
 setuptools.setup(
-    name = 'todo_catalog',
-    version = '0.2.0',
-    description='A command line application used to generate a TODO.md file for a given project.',
+    name="todo_catalog",
+    version="0.2.3",
+    description="A command line application used to generate a TODO.md file for a given project.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/supermanzer/todo-cla',
-    author='C. Ryan Manzer',
-    author_email='ryan.manzer@gmail.com',
-    packages = ['todo_catalog'],
-    install_requires = [
-        'Click',
-        'configparser',
+    long_description_content_type="text/markdown",
+    url="https://github.com/supermanzer/todo-cla",
+    author="C. Ryan Manzer",
+    author_email="ryan.manzer@gmail.com",
+    packages=["todo_catalog"],
+    install_requires=[
+        "Click",
+        "configparser",
     ],
-    entry_points = '''
+    entry_points="""
         [console_scripts]
         todo_catalog=todo_catalog.todo_catalog:find_todos
-    '''
+    """,
 )
--- todo_catalog/todo_catalog.py
+++ todo_catalog/todo_catalog.py
@@ -5,29 +5,45 @@ import os
 import sys
 import re
 
+
 def get_config(dir):
-  """
-  A function to parse the config file, if present.
-  """
-  result = {}
-  config = configparser.ConfigParser()
+    """
+    A function to parse the config file, if present.
+    """
+    result = {}
+    config = configparser.ConfigParser()
 
-  if os.path.isfile(os.path.join(dir,'config')):
-      config.read(os.path.join(dir,'config'))
-      if config.has_option('SETTINGS', 'file_ext'):
-        result['file_ext'] = [x.strip() for x in config['SETTINGS']['file_ext'].split(',')]
-      if config.has_option('SETTINGS', 'files_to_ignore'):
-        result['files_to_ignore'] = [x.strip() for x in config['SETTINGS']['files_to_ignore'].split(',')]
-      if config.has_option('SETTINGS','dirs_to_ignore'):
-        result['dirs_to_ignore'] = [x.strip() for x in config['SETTINGS']['dirs_to_ignore'].split(',')]
+    if os.path.isfile(os.path.join(dir, "config")):
+        config.read(os.path.join(dir, "config"))
+        if config.has_option("SETTINGS", "file_ext"):
+            result["file_ext"] = [
+                x.strip() for x in config["SETTINGS"]["file_ext"].split(",")
+            ]
+        if config.has_option("SETTINGS", "files_to_ignore"):
+            result["files_to_ignore"] = [
+                x.strip() for x in config["SETTINGS"]["files_to_ignore"].split(",")
+            ]
+        if config.has_option("SETTINGS", "dirs_to_ignore"):
+            result["dirs_to_ignore"] = [
+                x.strip() for x in config["SETTINGS"]["dirs_to_ignore"].split(",")
+            ]
 
-  return result
+    return result
+
 
 @click.command()
-@click.option('--dir', default=False, help='root directory')
-@click.option('--file_ext', default=False, help='File extensions to scan')
-@click.option('--files_to_ignore', default=[], help='Files to ignore while scanning for TODO comments.')
-@click.option('--dirs_to_ignore', default=[], help='Directories to ignore while scanning for TODO comments.')
+@click.option("--dir", default="", help="root directory")
+@click.option("--file_ext", default="", help="File extensions to scan")
+@click.option(
+    "--files_to_ignore",
+    default=[],
+    help="Files to ignore while scanning for TODO comments.",
+)
+@click.option(
+    "--dirs_to_ignore",
+    default=[],
+    help="Directories to ignore while scanning for TODO comments.",
+)
 def find_todos(dir, file_ext, files_to_ignore, dirs_to_ignore):
     """
     A command line function for walking the directory of a project, scanning the code files, finding TODO statements, and building a catalogue of them. A directory can be passed as the first command line argument to initiate the process somewhere other than the current working directory.  It will create or overwrite a TODO.md document in the root folder.
@@ -49,6 +65,9 @@ def find_todos(dir, file_ext, files_to_ignore, dirs_to_ignore):
     EXAMPLE:
         todo_catalog --file_ext=('.py', '.js', '.css', '.html') --dirs_to_ignore = ['node_modules']
     """
+    import pdb
+
+    pdb.set_trace()
     # Checking to see if a directory was passed in
     if dir:
         root = dir
@@ -58,26 +77,26 @@ def find_todos(dir, file_ext, files_to_ignore, dirs_to_ignore):
     # Checking for and parsing our config file
     conf = get_config(root)
 
-     # File extensions we will pay attention to.  -- THIS SHOULD BE REFACTORED ONCE WE CAN VERIFY PROPER FUNCTIONAITY
-    if not file_ext:
-        if 'file_ext' in conf.keys():
-            file_ext = conf['file_ext']
+    # File extensions we will pay attention to.  -- THIS SHOULD BE REFACTORED ONCE WE CAN VERIFY PROPER FUNCTIONAITY
+    if "file_ext" in conf.keys():
+        if not file_ext:
+            file_ext = conf["file_ext"]
         else:
-            file_ext = ('.py', '.txt', '.php', '.js', '.css', '.html')
+            file_ext = (".py", ".txt", ".php", ".js", ".css", ".html")
 
-    if not files_to_ignore:
-        if 'files_to_ignore' in conf.keys():
-            files_to_ignore = conf['files_to_ignore']
-            
-    if not dirs_to_ignore:
-        if 'dirs_to_ignore' in conf.keys():
-          dirs_to_ignore = conf['dirs_to_ignore']
+    if "files_to_ignore" in conf.keys():
+        if not files_to_ignore:
+            files_to_ignore = conf["files_to_ignore"]
+
+    if "dirs_to_ignore" in conf.keys():
+        if not dirs_to_ignore:
+            dirs_to_ignore = conf["dirs_to_ignore"]
 
     #  Writing our TODO.md file
-    td=open(os.path.join(root,'TODO.md'),'w+')
+    td = open(os.path.join(root, "TODO.md"), "w+")
 
     # Walking our directory tree with a counter variable
-    k=1
+    k = 1
     for (dirname, dirs, files) in os.walk(root, topdown=True):
         dirs[:] = [d for d in dirs if d not in dirs_to_ignore]
         files[:] = [f for f in files if f not in files_to_ignore]
@@ -89,17 +108,24 @@ def find_todos(dir, file_ext, files_to_ignore, dirs_to_ignore):
                 # run.  So we truncate the print_file variable to
                 # only include file paths relative to the root
                 # directory.
-                print_file=os.path.join(dirname,filename)
-                print_file=print_file.replace(root+'/','')
-                with open(os.path.join(dirname,filename),'r') as f:
+                print_file = os.path.join(dirname, filename)
+                print_file = print_file.replace(root + "/", "")
+                with open(os.path.join(dirname, filename), "r") as f:
                     for n, line in enumerate(f, 1):
                         if "TODO" in line:
-                            mObj=re.match(r'.*TODO:*\s*(.*)',line)
-                            out_text= "* %i) %s line %i - %s \n" % (k,print_file,n,mObj.group(1))
+                            mObj = re.match(r".*TODO:*\s*(.*)", line)
+                            out_text = "* %i) %s line %i - %s \n" % (
+                                k,
+                                print_file,
+                                n,
+                                mObj.group(1),
+                            )
                             td.write(out_text)
-                            k+=1
+                            k += 1
 
     # If we didn't find anything, we remove the file
     if k == 1:
-      td.write('No TODO comments found within {}'.format(root))
-      print('No comments found.  If you think this is in error, check your configuration settings.')
+        td.write("No TODO comments found within {}".format(root))
+        print(
+            "No comments found.  If you think this is in error, check your configuration settings."
+        )
